93f9e1508b5a3552a88189a826e3e6f2a2751743
Djamali Ali bakar <dalibakar@gmail.com>
2024-09-07 20:29:52 +0200

    First commit

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "script/DeployBasicNft.s.sol";
+import {BasicNft} from "src/BasicNft.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer ;
+    BasicNft public basicNft;
+    address public USER  = makeAddr("user");
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public  {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory basicName =basicNft.name();
+        //assertEq(expectedName, basicName);
+        assert(keccak256(abi.encodePacked(expectedName))== keccak256(abi.encodePacked(basicName)));
+
+
+    }
+
+    function testMintHasBalance() public {
+        vm.prank(USER);
+        basicNft.minNft(PUG);
+
+        assert(basicNft.balanceOf(USER) ==1);
+        assert(keccak256(abi.encodePacked(PUG)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
+    }
+    
+
+}
