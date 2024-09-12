d81bc9a74fee76b3f30e068bfb97123fb27dc08c
Djamali Ali bakar <dalibakar@gmail.com>
2024-09-12 17:38:31 +0200

    Add more tests

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from  "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
 
